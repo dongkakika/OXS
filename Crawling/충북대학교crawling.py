2fbd1e44f77d9afade4e4713e57402a1f468ff03
senajang <cac02152l@gmail.com>
2020-05-05 00:27:25 +0900

    장세나 test

--- Crawling/충북대학교crawling.py
+++ Crawling/충북대학교crawling.py
@@ -3,8 +3,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 Url = "https://www.chungbuk.ac.kr/site/www/boardList.do?boardSeq=113&key=699"
-
-print(url)
+print(Url)
 
 driver = webdriver.Chrome()
-driver.get(url)
+driver.get(Url)
