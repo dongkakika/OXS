71cb2e3a6a0da0da7a1ccb2cb63ef9284ceeea1d
senajang <cac02152l@gmail.com>
2020-05-05 00:16:12 +0900

    장세나 test

--- Crawling/crawling 1st.py
+++ Crawling/crawling 1st.py
@@ -1,6 +1,24 @@
-import urllib.reauest
+from urllib.parse import quote_plus
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
-url = "https://www.naver.com/"
-html = urllib.request.urlopen(url)
+baseUrl = "https://www.google.com/search?q="
+plusUrl = input("무엇을 검색할까요? : ")
+url = baseUrl + quote_plus(plusUrl)
 
-print(html.read())
+print(url)
+
+driver = webdriver.Chrome()
+driver.get(url)
+
+html = driver.page_source
+soup = BeautifulSoup(html)
+
+r = soup.select('.r')
+for i in r:
+    print(i.select_one('.LC20lb.DKV0Md').text)
+    print(i.select_one('.iUh30.bc.tjvcx').text)
+    print(i.a.attrs['href'])
+    print()
+
+driver.close()
