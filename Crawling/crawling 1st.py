5cd4ce8534b79be9ad6bdd8b4398547030576148
senajang <cac02152l@gmail.com>
2020-05-11 23:12:11 +0900

    장세나 test

--- Crawling/crawling 1st.py
+++ Crawling/crawling 1st.py
@@ -2,9 +2,7 @@ from urllib.parse import quote_plus
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-baseUrl = "https://www.google.com/search?q="
-plusUrl = input("무엇을 검색할까요? : ")
-url = baseUrl + quote_plus(plusUrl)
+url = "https://www.chungbuk.ac.kr/site/www/boardList.do?page=1&boardSeq=113&key=699"
 
 print(url)
 
@@ -14,11 +12,9 @@ driver.get(url)
 html = driver.page_source
 soup = BeautifulSoup(html)
 
-r = soup.select('.r')
-for i in r:
-    print(i.select_one('.LC20lb.DKV0Md').text)
-    print(i.select_one('.iUh30.bc.tjvcx').text)
-    print(i.a.attrs['href'])
+subject = soup.select('.subject')
+for i in subject:
+    print(i.a.attrs)
     print()
 
 driver.close()
