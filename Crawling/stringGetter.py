c7caa97efdd710219b76b5410cded4d455c42d3b
senajang <cac02152l@gmail.com>
2020-05-11 23:13:21 +0900

    장세나

--- Crawling/stringGetter.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def getPageString(url) :
-    data = requests.get(url)
-    return data.content
--- Crawling/충북대학교crawling.py
+++
@@ -1,4 +0,0 @@
-from Crawling.stringGetter import getPageString
-
-url = "https://www.chungbuk.ac.kr/site/www/boardList.do?page=1&boardSeq=113&key=699"
-print(getPageString(url))
